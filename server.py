2c6bffa044a4d015a81037f98d333f64eafaf3cc
julianknodt <julianknodt@gmail.com>
2018-04-04 01:29:36 -0400

    Added a health checkpoint

--- server.py
+++ server.py
@@ -18,6 +18,10 @@ def login():
   resp.set_cookie('ayobi_auth', "SAMPLE")
   return resp
 
+@app.route('/health')
+def health():
+  return "ALIVE"
+
 if __name__ == "__main__":
   port = int(os.environ.get('PORT', 5000))
   app.run(host='0.0.0.0', port=port, debug=True)
